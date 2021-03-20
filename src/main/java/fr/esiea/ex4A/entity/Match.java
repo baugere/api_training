4980d58483323e0d7b7ef064c530c8e8564d9c50
baptiste augere <augere.baptiste@gmail.com>
2021-03-20 17:27:12 +0100

    add class Match

---
+++ src/main/java/fr/esiea/ex4A/entity/Match.java
@@ -0,0 +1,19 @@
+package fr.esiea.ex4A.entity;
+
+public class Match {
+    private final String name;
+    private final String twitter;
+
+    public Match(String name, String twitter) {
+        this.name = name;
+        this.twitter = twitter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+}

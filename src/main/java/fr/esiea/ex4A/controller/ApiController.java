25c661dee5b5fb8944539ae33d213a6b194532a8
baptiste augere <augere.baptiste@gmail.com>
2021-03-20 16:08:39 +0100

    create user

--- src/main/java/fr/esiea/ex4A/controller/ApiController.java
+++ src/main/java/fr/esiea/ex4A/controller/ApiController.java
@@ -1,2 +1,22 @@
-package fr.esiea.ex4A.controller;public class ApiController {
+package fr.esiea.ex4A.controller;
+
+import fr.esiea.ex4A.entity.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+@RestController
+public class ApiController {
+
+    private final ArrayList<User> userList = new ArrayList<>();
+
+    @PostMapping("/api/inscription")
+    public ResponseEntity<?> addUser(@RequestBody Map<String, String> request){
+        User user = new User(request.get("userEmail"), request.get("userName"), request.get("userTweeter"), request.get("userCountry"), request.get("userSex"), request.get("userSexPref"));
+        userList.add(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
 }

32e959a5c243654105ef3dd6948b4c4bbddf6938
baptiste augere <augere.baptiste@gmail.com>
2021-03-21 18:54:20 +0100

    agify client bean

--- src/main/java/fr/esiea/ex4A/Launcher.java
+++ src/main/java/fr/esiea/ex4A/Launcher.java
@@ -1,7 +1,12 @@
 package fr.esiea.ex4A;
 
+import fr.esiea.ex4A.agify.AgifyClient;
+import okhttp3.OkHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @SpringBootApplication
 public class Launcher {
@@ -9,4 +14,18 @@ public class Launcher {
     public static void main(String[] args) {
         SpringApplication.run(Launcher.class, args);
     }
+
+    @Bean
+    AgifyClient agifyClient() {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+        Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(AgifyClient.BASE_URI)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient.build())
+            .build();
+
+        return retrofit.create(AgifyClient.class);
+    }
+
 }

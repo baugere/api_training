8e343b60a657dd9dd10fb172c3bc66b06f5e3664
baptiste augere <augere.baptiste@gmail.com>
2021-03-21 20:07:23 +0100

    add agify client

---
+++ src/main/java/fr/esiea/ex4A/agify/AgifyClient.java
@@ -0,0 +1,17 @@
+package fr.esiea.ex4A.agify;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+import java.util.Map;
+
+public interface AgifyClient {
+    public static final String BASE_URI = "https://api.agify.io";
+
+    @GET("/")
+    Call<Map<String, String>> getAge(@Query("name") String name, @Query("country_id") String countryId);
+
+    
+
+}

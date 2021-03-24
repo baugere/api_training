4ef845671e09483c6b6d962dede11b3b301f7faa
baptiste augere <augere.baptiste@gmail.com>
2021-03-24 21:37:37 +0100

    add test for agify service

---
+++ src/main/java/fr/esiea/ex4A/agify/mock/AgifyServiceMock.java
@@ -0,0 +1,21 @@
+package fr.esiea.ex4A.agify.mock;
+
+
+import fr.esiea.ex4A.agify.AgifyClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.mock.BehaviorDelegate;
+import retrofit2.mock.MockRetrofit;
+import retrofit2.mock.NetworkBehavior;
+
+public class AgifyServiceMock {
+    public static AgifyClient getAgifyClientMock() {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(AgifyClient.BASE_URI).addConverterFactory(GsonConverterFactory.create()).build();
+
+        NetworkBehavior networkBehavior = NetworkBehavior.create();
+        MockRetrofit mockRetrofit = new MockRetrofit.Builder(retrofit).networkBehavior(networkBehavior).build();
+        BehaviorDelegate<AgifyClient> delegate = mockRetrofit.create(AgifyClient.class);
+
+        return new AgifyClientMockImpl(delegate);
+    }
+}
--- src/test/java/fr/esiea/ex4A/agify/AgifyServiceTest.java
+++ src/test/java/fr/esiea/ex4A/agify/AgifyServiceTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package fr.esiea.ex4A.agify;
+
+import fr.esiea.ex4A.agify.mock.AgifyServiceMock;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class AgifyServiceTest {
-  
+    @Mock
+    private final AgifyService agifyService;
+
+    AgifyServiceTest() {
+        this.agifyService = new AgifyService(AgifyServiceMock.getAgifyClientMock());
+    }
+
+    @Test
+    void getAgeOfJeanFr() throws IOException {
+        assertThat(agifyService.getAge("jean", "FR")).isEqualTo(68);
+    }
 }
